d1547ae60b2cd3018b913624c60725823c574691
luthfiharw <luthfiharw9@gmail.com>
2024-09-08 01:34:23 +0700

    s

--- cpu.v
+++ cpu.v
@@ -1,23 +1,33 @@
-module cpu (clk, reset, instruction, data_out);
+module CPU (clk, reset, A, B, pc_out, alu_result);
 input clk;
 input reset;
-input [7:0] instruction;
-output reg [7:0] data_out;
+input [3:0] A, B;
+output [7:0] pc_out;
+output [3:0] alu_result;
 
-//register declaration
+wire [1:0] alu_op;
+wire [3:0] opcode = 4'b0001;
 
-//ALU?
+ControlUnit CU (
+    .opcode(opcode),
+    .reg_write(),
+    .mem_read(),
+    .mem_write(),
+    .alu_op(alu_op)
+);
 
-//control unit
+ALU myALU (
+    .A(A),
+    .B(B),
+    .ALUop(alu_op),
+    .Result(alu_result)
+);
 
-//pc (program counter)
-
-//data memory
-
-//instruction memory
-
-always @(*) begin
-    //top
-end
+ProgramCounter myPC (
+    .clk(clk),
+    .reset(reset),
+    .pc_in(alu_result),
+    .pc_out(pc_out)
+);
 
 endmodule
