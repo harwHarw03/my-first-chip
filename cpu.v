0abd1d7e149321ce447e940fe830e3a964cf72c0
luthfiharw <luthfiharw9@gmail.com>
2024-09-07 23:57:56 +0700

    cpu, progress

---
+++ cpu.v
@@ -0,0 +1,23 @@
+module cpu (clk, reset, instruction, data_out);
+input clk;
+input reset;
+input [7:0] instruction;
+output reg [7:0] data_out;
+
+//register declaration
+
+//ALU?
+
+//control unit
+
+//pc (program counter)
+
+//data memory
+
+//instruction memory
+
+always @(*) begin
+    //top
+end
+
+endmodule
