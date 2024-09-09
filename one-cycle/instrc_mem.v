b215bf5320d4d1cd86e47b85739ebf3cea760d96
harwHarw03 <luthfiharw9@gmail.com>
2024-09-09 16:03:56 +0700

    so

--- one-cycle/instrc_mem.v
+++ one-cycle/instrc_mem.v
@@ -5,9 +5,10 @@ output reg [15:0] instruction;
 reg [15:0] memory [0:255]; // instruction memory size (256 words, 16-bit)
 
 initial begin 
-//example instructions
-    memory[0] = 16'b0001_0000_0001_0010; //(opcode + operands)
-    memory[1] = 16'b0010_0010_0001_0100;
+    // Example instructions
+    memory[0] = 16'b0001_0000_0001_0010; // ADD A, B
+    memory[1] = 16'b0011_0010_0010_0000; // SUBTRACT (result, B)
+    memory[2] = 16'b0100_0010_0011_0000; // AND (result, B)
 end
 
 always @(*) begin 
